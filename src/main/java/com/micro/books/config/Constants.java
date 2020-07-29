f7ecbeda93b4ffa7c23eaf423dc3bd9a82a19278
Arun K Abraham <151123@ust-global.com>
2020-07-29 22:46:51 +0530

    Initial version of books generated by JHipster-6.10.1

---
+++ src/main/java/com/micro/books/config/Constants.java
@@ -0,0 +1,12 @@
+package com.micro.books.config;
+
+/**
+ * Application constants.
+ */
+public final class Constants {
+
+    public static final String SYSTEM_ACCOUNT = "system";
+
+    private Constants() {
+    }
+}
---
+++ src/main/java/com/micro/books/config/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Framework configuration files.
+ */
+package com.micro.books.config;
